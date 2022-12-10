104fb777e0aa23c79f83c2a7c6bf3555f675382a
keikur1hara <kurihara.k2hit@gmail.com>
2022-12-10 12:23:02 +0900

    配列

--- hello.go
+++ hello.go
@@ -5,13 +5,28 @@ import (
 )
 
 func main() {
-	byteA := []byte{72, 73}
-	fmt.Println(byteA)
+	var arri [3]int
+	fmt.Println(arri)
 
-	fmt.Println(string(byteA))
+	fmt.Printf("%T\n", arri)
 
-	c := []byte("HI")
-	fmt.Println(c)
+	var arri2 [3]string = [3]string{"A", "B"}
+	fmt.Println(arri2)
 
-	fmt.Println(string(c))
+	arri3 := [3]int{1, 2, 3}
+	fmt.Println(arri3)
+
+	arri4 := [...]string{"C", "D"}
+	fmt.Println(arri4)
+	fmt.Printf("%T\n", arri4)
+
+	fmt.Println(arri2[0])
+	fmt.Println(arri2[1])
+	fmt.Println(arri2[2-1])
+
+	arri2[2] = "C"
+	fmt.Println(arri2)
+
+	fmt.Println(len(arri))
+
 }
