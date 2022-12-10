428a7fe87f7e32b8c9321bd08e786d0a06f982a8
keikur1hara <kurihara.k2hit@gmail.com>
2022-12-10 12:11:02 +0900

    string

--- hello.go
+++ hello.go
@@ -5,32 +5,19 @@ import (
 )
 
 func main() {
-	var i int = 100
-	var i2 int64 = 200
+	var t, f bool = true, false
+	fmt.Println(t, f)
 
-	fmt.Println(i + 50)
+	var s string = "Hello"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
 
-	fmt.Printf("%T\n", i2)
-	fmt.Printf("%T\n", int32(i2))
-	fmt.Print(int32(i) + int32(i2))
+	var si string = "test"
+	fmt.Println(si)
+	fmt.Printf("%T\n", si)
 
-	var fl64 float64 = 2.4
-	fmt.Println(fl64)
+	fmt.Println("\"")
 
-	fl := 3.2
-	fmt.Printf("%T\n", fl)
-	fmt.Printf("%T\n", fl64)
-
-	var fl32 float32 = 1.2
-	fmt.Printf("%T\n", fl32)
-
-	zero := 0.0
-	pinf := 1.0
-	fmt.Println(pinf)
-
-	ninf := -1.0 / zero
-	fmt.Println(ninf)
-
-	nan := zero / zero
-	fmt.Println(nan)
+	fmt.Println(s[0])
+	fmt.Println(string(s[0]))
 }
