d67118349ba1f6eea27c99b5010c35177af34fab
keikur1hara <kurihara.k2hit@gmail.com>
2022-12-10 13:42:26 +0900

    定数

--- hello.go
+++ hello.go
@@ -1,56 +1,29 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
+import "fmt"
+
+const Pi = 3.14
+
+const (
+	URL      = "http://google.com"
+	SiteName = "Google"
 )
 
+const (
+	A = iota
+	B
+	C
+	D = "A"
+	E
+	F
+)
+
+var Big int
+
 func main() {
-	// var x interface{}
-	// fmt.Println(x)
+	fmt.Println(Pi)
+	fmt.Println(URL, SiteName)
 
-	// x = 1
-	// fmt.Println(x)
-
-	// x = 3.14
-	// fmt.Println(x)
-
-	// x = "string"
-	// fmt.Println(x)
-
-	// var i int = 1
-	// fl64 := float64(i)
-
-	// fmt.Println(fl64)
-	// fmt.Printf("%T\n", i)
-	// fmt.Printf("%T\n", fl64)
-
-	// i2 := int(fl64)
-	// fmt.Printf("%T\n", i2)
-
-	// fl := 10.5
-	// i3 := int(fl)
-
-	// fmt.Printf("%T\n", i3)
-	// fmt.Printf("%T\n", fl)
-
-	var s string = "100"
-	fmt.Printf("s = %T\n", s)
-
-	i, _ := strconv.Atoi(s)
-	fmt.Println(i)
-	fmt.Printf("i = %T\n", i)
-
-	var i2 int = 200
-	s2 := strconv.Itoa(i2)
-	fmt.Println(s2)
-	fmt.Printf("s2 = %T\n", s2)
-
-	var h string = "heelo world"
-	b := []byte(h)
-	fmt.Println(b)
-
-	h2 := string(b)
-	fmt.Println(h2)
+	fmt.Println(A, B, C, D, E, F)
 
 }
