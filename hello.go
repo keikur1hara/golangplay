7eba705f4847be335d36d05cb1b3e404c29e46f0
keikur1hara <kurihara.k2hit@gmail.com>
2022-12-13 22:50:33 +0900

    for文

--- hello.go
+++ hello.go
@@ -1,20 +1,52 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 func main() {
+	i := 0
 
-	var s string = "100"
+	for {
 
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		fmt.Println(err)
-
+		fmt.Println("Loop")
+		i++
+		if i == 3 {
+			break
+		}
 	}
 
-	fmt.Printf("i = %T\n", i)
+	point := 0
+	for point < 10 {
+		fmt.Println(point)
+		point++
+	}
 
+	for i := 0; i < 10; i++ {
+		if i == 3 {
+			continue
+		}
+		if i == 6 {
+			break
+		}
+		fmt.Println(i)
+	}
+
+	arr := [3]int{1, 2, 3}
+
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+	}
+
+	for _, v := range arr {
+		fmt.Println(v)
+	}
+
+	sl := []string{"Python", "PHP", "GO"}
+	for i, v := range sl {
+		fmt.Println(i, v)
+	}
+
+	m := map[string]int{"apple": 100, "banana": 200}
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
 }
