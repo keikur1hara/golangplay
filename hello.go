ecbfd48f8171d5f2a22992f0d0fc967f3eafb9ba
keikur1hara <kurihara.k2hit@gmail.com>
2022-12-13 23:20:21 +0900

    switch 型スイッチ

--- hello.go
+++ hello.go
@@ -4,40 +4,54 @@ import (
 	"fmt"
 )
 
+func anything(a interface{}) {
+	// fmt.Println(a)
+	switch v := a.(type) {
+	case string:
+		fmt.Println(v + "!?")
+	case int:
+		fmt.Println(v + 10000)
+
+	}
+}
+
 func main() {
-	// n := 5
-	// switch n {
-	// case 1, 2:
-	// 	fmt.Println("1 or 2")
-	// case 3, 4:
-	// 	fmt.Println("3 or 4")
-	// default:
-	// 	fmt.Println("I dont know")
-	// }
+	anything("aaa")
+	anything(1)
 
-	// switch n := 2; n {
-	// case 1, 2:
-	// 	fmt.Println("1 or 2")
-	// case 3, 4:
-	// 	fmt.Println("3 or 4")
-	// default:
-	// 	fmt.Println("I dont know")
-	// }
+	var x interface{} = 3
+	// i, isInt := x.(int)
+	// fmt.Println(i+2, isInt)
 
-	// n := 6
-	// switch {
-	// case n > 0 && n < 4:
-	// 	fmt.Println("0 < n < 4")
-	// case n > 3 && n < 7:
-	// 	fmt.Println("3 < n < 7")
-	// }
+	f, isFloat64 := x.(float64)
+	fmt.Println(f, isFloat64)
 
-	switch n := 2; n {
-	case 1, 2:
-		fmt.Println("1 or 2")
-	case 3, 4:
-		fmt.Println("3 or 4")
+	if x == nil {
+		fmt.Println("None")
+	} else if i, isInt := x.(int); isInt {
+		fmt.Println(i, "x is int")
+	} else if s, isString := x.(string); isString {
+		fmt.Println(s, isString)
+	} else {
+		fmt.Println("i dont know")
+	}
+
+	switch x.(type) {
+	case int:
+		fmt.Println("int")
+	case string:
+		fmt.Println("string")
 	default:
-		fmt.Println("I dont know")
+		fmt.Println("i dont know")
+	}
+
+	switch v := x.(type) {
+	case bool:
+		fmt.Println(v, "bool")
+	case int:
+		fmt.Println(v, "int")
+	case string:
+		fmt.Println(v, "string")
+
 	}
 }
