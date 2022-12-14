a62d284fdbf49b12f2cf1e31512a7508df53ae3c
keikur1hara <kurihara.k2hit@gmail.com>
2022-12-15 00:15:37 +0900

    init

--- hello.go
+++ hello.go
@@ -5,18 +5,11 @@ import (
 	"time"
 )
 
-func sub() {
-	for {
-		fmt.Println(("Subloop"))
-		time.Sleep(100 * time.Millisecond)
-	}
+func init() {
+	fmt.Println(("Subloop"))
+	time.Sleep(100 * time.Millisecond)
 }
 func main() {
-	go sub()
-	go sub()
 
-	for {
-		fmt.Println("Main Loop")
-		time.Sleep(200 * time.Millisecond)
-	}
+	fmt.Println("Test")
 }
