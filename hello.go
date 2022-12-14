2bd1425c13da3077a46d3c1c04ea0e1f5ed29b4f
keikur1hara <kurihara.k2hit@gmail.com>
2022-12-15 00:09:22 +0900

    defer

--- hello.go
+++ hello.go
@@ -2,56 +2,37 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
-func anything(a interface{}) {
-	// fmt.Println(a)
-	switch v := a.(type) {
-	case string:
-		fmt.Println(v + "!?")
-	case int:
-		fmt.Println(v + 10000)
+func TestDefer() {
+	defer fmt.Println("end")
+	fmt.Println("Start")
+}
 
-	}
+func RunDefer() {
+	defer fmt.Println("1")
+	defer fmt.Println("2")
+	defer fmt.Println("3")
+
 }
 
 func main() {
-	anything("aaa")
-	anything(1)
+	TestDefer()
 
-	var x interface{} = 3
-	// i, isInt := x.(int)
-	// fmt.Println(i+2, isInt)
+	defer func() {
+		fmt.Println("1")
+		fmt.Println("2")
+		fmt.Println("3")
+	}()
 
-	f, isFloat64 := x.(float64)
-	fmt.Println(f, isFloat64)
+	RunDefer()
 
-	if x == nil {
-		fmt.Println("None")
-	} else if i, isInt := x.(int); isInt {
-		fmt.Println(i, "x is int")
-	} else if s, isString := x.(string); isString {
-		fmt.Println(s, isString)
-	} else {
-		fmt.Println("i dont know")
+	file, err := os.Create("test.txt")
+	if err != nil {
+		fmt.Println(err)
 	}
+	defer file.Close()
 
-	switch x.(type) {
-	case int:
-		fmt.Println("int")
-	case string:
-		fmt.Println("string")
-	default:
-		fmt.Println("i dont know")
-	}
-
-	switch v := x.(type) {
-	case bool:
-		fmt.Println(v, "bool")
-	case int:
-		fmt.Println(v, "int")
-	case string:
-		fmt.Println(v, "string")
-
-	}
+	file.Write([]byte("Hello"))
 }
