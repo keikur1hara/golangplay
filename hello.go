acee5ee0e46786d7d9e96f2e526f41cf3579f60c
keikur1hara <kurihara.k2hit@gmail.com>
2023-01-12 20:55:45 +0900

    スライス　append

--- hello.go
+++ hello.go
@@ -5,31 +5,26 @@ import (
 )
 
 func main() {
-	var sl []int
-
+	sl := []int{100, 200}
 	fmt.Println(sl)
 
-	var sl2 []int = []int{100, 200}
+	sl = append(sl, 300)
+	fmt.Println(sl)
+
+	sl = append(sl, 400, 500, 600)
+	fmt.Println(sl)
+
+	sl2 := make([]int, 5, 10)
 	fmt.Println(sl2)
 
-	var sl3 []string = []string{"A", "B"}
+	fmt.Println(len(sl2))
+
+	fmt.Println(cap(sl2))
+
+	sl3 := make([]int, 5, 10)
+	sl3 = append(sl3, 1, 2, 3, 4, 5, 6, 7)
 	fmt.Println(sl3)
-
-	sl4 := make([]int, 5)
-	fmt.Println(sl4)
-
-	sl2[0] = 1000
-	fmt.Println(sl2)
-
-	sl5 := []int{1, 2, 3, 4, 5}
-	fmt.Println(sl5)
-
-	fmt.Println(sl5[0])
-
-	fmt.Println(sl5[2:4])
-
-	fmt.Println(sl5[:2])
-
-	fmt.Println(sl5[2:])
+	fmt.Println(len(sl3))
+	fmt.Println(cap(sl3))
 
 }
