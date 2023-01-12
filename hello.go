b05dfbd105e2ec74d3d66e7f06980632bf21d8e3
keikur1hara <kurihara.k2hit@gmail.com>
2023-01-12 20:49:10 +0900

    スライス

--- hello.go
+++ hello.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func init() {
-	fmt.Println(("Subloop"))
-	time.Sleep(100 * time.Millisecond)
-}
 func main() {
+	var sl []int
 
-	fmt.Println("Test")
+	fmt.Println(sl)
+
+	var sl2 []int = []int{100, 200}
+	fmt.Println(sl2)
+
+	var sl3 []string = []string{"A", "B"}
+	fmt.Println(sl3)
+
+	sl4 := make([]int, 5)
+	fmt.Println(sl4)
+
+	sl2[0] = 1000
+	fmt.Println(sl2)
+
+	sl5 := []int{1, 2, 3, 4, 5}
+	fmt.Println(sl5)
+
+	fmt.Println(sl5[0])
+
+	fmt.Println(sl5[2:4])
+
+	fmt.Println(sl5[:2])
+
+	fmt.Println(sl5[2:])
+
 }
