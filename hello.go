abcec319b9d651d87b7864bf56fdd6e6da773bd7
keikur1hara <kurihara.k2hit@gmail.com>
2023-01-18 03:07:19 +0900

    map for

--- hello.go
+++ hello.go
@@ -4,42 +4,16 @@ import "fmt"
 
 func main() {
 
-	var m = map[string]int{"A": 100, "B": 200}
-	fmt.Println(m)
-
-	m2 := map[string]int{"A": 100, "B": 200}
-
-	fmt.Println(m2)
-
-	m3 := map[int]string{
-		1: "A",
-		2: "B",
+	m := map[string]int{
+		"Apple":  100,
+		"banana": 200,
 	}
 
-	fmt.Println(m3)
-
-	m4 := make(map[int]string)
-	fmt.Println(m4)
-
-	m4[1] = "JAPAN"
-	m4[2] = "USA"
-	fmt.Println(m4)
-
-	fmt.Println(m["A"])
-	fmt.Println(m4[2])
-	fmt.Println(m4[3])
-
-	s, ok := m4[4]
-	fmt.Println(s)
-	if !ok {
-		fmt.Println("error")
+	for k, v := range m {
+		fmt.Println(k, v)
 	}
-	fmt.Println(s, ok)
-
-	m3[3] = "aa"
-	fmt.Println(m3)
-
-	delete(m3, 3)
-	fmt.Println(m3)
+	for k := range m {
+		fmt.Println(m[k])
+	}
 
 }
