b594292e9e749165310924cbc30f86538af01f7f
keikur1hara <kurihara.k2hit@gmail.com>
2023-01-18 21:47:24 +0900

    chanel close

--- hello.go
+++ hello.go
@@ -5,26 +5,40 @@ import (
 	"time"
 )
 
-func reciever(c chan int) {
+func reciever(name string, ch chan int) {
 	for {
-		i := <-c
-		fmt.Println(i)
+		i, ok := <-ch
+		if !ok {
+			break
+		}
+		fmt.Println(name, i)
 	}
+	fmt.Println(name + "END")
 }
 
 func main() {
+	ch1 := make(chan int, 2)
 
-	ch1 := make(chan int)
-	ch2 := make(chan int)
+	// ch1 <- 1
 
-	go reciever(ch1)
-	go reciever(ch2)
+	// close(ch1)
+
+	// i, ok := <-ch1
+	// fmt.Println(i,ok)
+
+	// i2,ok := <-ch1
+	// fmt.Println(i,ok)
+
+	go reciever("1.goroutin", ch1)
+	go reciever("2.goroutin", ch1)
+	go reciever("3.goroutin", ch1)
 
 	i := 0
 	for i < 100 {
 		ch1 <- i
-		ch2 <- i
-		time.Sleep(50 * time.Millisecond)
 		i++
 	}
+	close(ch1)
+	time.Sleep(3 * time.Second)
+
 }
