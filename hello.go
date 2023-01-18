22fac6ac642858c431d79bf852c019cfd9e5c7d0
keikur1hara <kurihara.k2hit@gmail.com>
2023-01-18 21:12:08 +0900

    chanel

--- hello.go
+++ hello.go
@@ -3,17 +3,42 @@ package main
 import "fmt"
 
 func main() {
+	var ch1 chan int
 
-	m := map[string]int{
-		"Apple":  100,
-		"banana": 200,
-	}
+	//受信戦湯王
+	// var ch2 <-chan int
+	//送信専用
+	// var ch3 chan<- int
 
-	for k, v := range m {
-		fmt.Println(k, v)
-	}
-	for k := range m {
-		fmt.Println(m[k])
-	}
+	ch1 = make(chan int)
+	ch2 := make(chan int)
 
+	fmt.Println(cap(ch1))
+	fmt.Println(cap(ch2))
+
+	ch3 := make(chan int, 5)
+	fmt.Println(cap(ch3))
+
+	ch3 <- 1
+	fmt.Println(len(ch3))
+
+	ch3 <- 5
+	ch3 <- 3
+	fmt.Println(len(ch3))
+
+	i := <-ch3
+	fmt.Println(i)
+	i2 := <-ch3
+	fmt.Println(i2)
+	fmt.Println("len", len(ch3))
+
+	fmt.Println(<-ch3)
+	fmt.Println("len", len(ch3))
+	ch3 <- 1
+	fmt.Println(<-ch3)
+	ch3 <- 2
+	ch3 <- 3
+	ch3 <- 4
+	ch3 <- 5
+	ch3 <- 6
 }
